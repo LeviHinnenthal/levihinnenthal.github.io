c78b58040c4ca8a2a19fbd98601213c813c2edc3
LeviHinnenthal <106553886+LeviHinnenthal@users.noreply.github.com>
2022-08-27 15:04:44 -0300

    Update index.js

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -3,7 +3,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     
-if(document.URL == "https://levihinnenthal.github.io/"){
+ if(document.URL == "https://levihinnenthal.github.io"){
     window.location.href = 'https://levihinnenthal.github.io/protected/views/index.html';
 }
 });
