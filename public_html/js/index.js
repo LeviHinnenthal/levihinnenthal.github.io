8be7d7e4090629d67b359a9f1b77106fc9b56ac7
LeviHinnenthal <106553886+LeviHinnenthal@users.noreply.github.com>
2022-08-27 15:04:17 -0300

    documetn ready

---
+++ public_html/js/index.js
@@ -0,0 +1,10 @@
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+if(document.URL == "https://levihinnenthal.github.io/"){
+    window.location.href = 'https://levihinnenthal.github.io/protected/views/index.html';
+}
+});
+
